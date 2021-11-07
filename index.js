55c34189497bf01dd007939e76b29021111d3b66
Yontida Yosbut <a.yontida@gmail.com>
2021-11-07 18:51:10 -0500

    commit tree a61807a38b5d130965eb086ca5cbf165d519b255

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+function returnFirstTwoDrivers(){
+  const firstTwoDrivers = drivers.slice(0,2)
+    return firstTwoDrivers;
+}
+
+function returnLastTwoDrivers(){
+  const lastTwoDrivers = drivers.slice(2,4)
+   return lastTwoDrivers;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+function createFareMultiplier(integer){
+  return function (){
+    return integer * integer;
+  };
+}
+
+const fareDoubler = fare => fare * 2;
+const fareTripler = fare => fare * 3;
+
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers){
+  return returnFirstTwoDrivers(drivers);
+}
+
+// console.log(returnFirstTwoDrivers(drivers))
